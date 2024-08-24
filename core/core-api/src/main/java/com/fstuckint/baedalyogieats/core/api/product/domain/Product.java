c4df75ad752cfb573824252c72b7fe01221935c8
Hyyena <sjtxm0320@gmail.com>
2024-08-24 16:18:33 +0900

    ✨ feat: 상품 도메인 추가

---
+++ core/core-api/src/main/java/com/fstuckint/baedalyogieats/core/api/product/domain/Product.java
@@ -0,0 +1,11 @@
+package com.fstuckint.baedalyogieats.core.api.product.domain;
+
+import com.fstuckint.baedalyogieats.storage.db.core.product.ProductEntity;
+import java.util.UUID;
+
+public record Product(String name, String description, int unitPrice, UUID storeUuid) {
+
+	public ProductEntity toEntity() {
+		return new ProductEntity(name, description, unitPrice, storeUuid);
+	}
+}
---
+++ storage/db-core/src/main/java/com/fstuckint/baedalyogieats/storage/db/core/product/ProductEntity.java
@@ -0,0 +1,51 @@
+package com.fstuckint.baedalyogieats.storage.db.core.product;
+
+import com.fstuckint.baedalyogieats.storage.db.core.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_products")
+public class ProductEntity extends BaseEntity {
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@Column(nullable = false)
+	private int unitPrice;
+
+	@Column(nullable = false)
+	private UUID storeUuid;
+
+	public ProductEntity() {
+	}
+
+	public ProductEntity(String name, String description, int unitPrice, UUID storeUuid) {
+		this.name = name;
+		this.description = description;
+		this.unitPrice = unitPrice;
+		this.storeUuid = storeUuid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public int getUnitPrice() {
+		return unitPrice;
+	}
+
+	public UUID getStoreUuid() {
+		return storeUuid;
+	}
+
+}
