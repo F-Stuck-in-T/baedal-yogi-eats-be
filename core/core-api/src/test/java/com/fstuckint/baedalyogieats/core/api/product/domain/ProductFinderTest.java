a2dfd09539de19aee76ee9f58fcd5b5a18f94243
Hyyena <sjtxm0320@gmail.com>
2024-08-25 00:40:23 +0900

    ✨ feat: 상품 상세 조회 비즈니스 로직 추가

--- core/core-api/src/test/java/com/fstuckint/baedalyogieats/core/api/product/domain/ProductFinderTest.java
+++ core/core-api/src/test/java/com/fstuckint/baedalyogieats/core/api/product/domain/ProductFinderTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.fstuckint.baedalyogieats.storage.db.core.product.ProductEntity;
+import com.fstuckint.baedalyogieats.storage.db.core.product.ProductRepository;
+import java.util.Optional;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -14,19 +16,16 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.util.ReflectionTestUtils;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class ProductServiceTest {
+class ProductFinderTest {
 
 	private UUID productUuid;
 
 	private UUID storeUuid;
 
 	@Mock
-	private ProductRegister productRegister;
+	private ProductRepository productRepository;
 
-	@Mock
 	private ProductFinder productFinder;
-
-	private ProductService productService;
 
 	@BeforeEach
 	void setUp() {
@@ -34,30 +33,7 @@ class ProductServiceTest {
 		this.storeUuid = UUID.randomUUID();
 
 		MockitoAnnotations.openMocks(this);
-		productService = new ProductService(productRegister, productFinder);
-	}
-
-	@Test
-	void 상품을_추가한다() {
-		// given
-		String name = "상품";
-		String description = "상품 설명";
-		int unitPrice = 10_000;
-		Product product = new Product(name, description, unitPrice, storeUuid);
-		ProductEntity productEntity = product.toEntity();
-		ReflectionTestUtils.setField(productEntity, "uuid", productUuid);
-		when(productRegister.add(product)).thenReturn(ProductResult.of(productEntity));
-
-		// when
-		ProductResult result = productService.register(product);
-
-		// then
-		assertThat(result).isNotNull();
-		assertThat(result.uuid()).isEqualTo(productUuid);
-		assertThat(result.name()).isEqualTo(name);
-		assertThat(result.description()).isEqualTo(description);
-		assertThat(result.unitPrice()).isEqualTo(unitPrice);
-		assertThat(result.storeUuid()).isEqualTo(storeUuid);
+		productFinder = new ProductFinder(productRepository);
 	}
 
 	@Test
@@ -69,10 +45,10 @@ class ProductServiceTest {
 		Product product = new Product(name, description, unitPrice, storeUuid);
 		ProductEntity productEntity = product.toEntity();
 		ReflectionTestUtils.setField(productEntity, "uuid", productUuid);
-		when(productFinder.find(productUuid)).thenReturn(ProductResult.of(productEntity));
+		when(productRepository.findByUuid(productUuid)).thenReturn(Optional.of(productEntity));
 
 		// when
-		ProductResult result = productService.find(productUuid);
+		ProductResult result = productFinder.find(productUuid);
 
 		// then
 		assertThat(result).isNotNull();
