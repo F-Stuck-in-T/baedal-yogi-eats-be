dc9182741ff6784af9024b4baa52fbdf3643a71f
Hyyena <sjtxm0320@gmail.com>
2024-09-02 01:19:54 +0900

    ✨ feat: querydsl을 사용한 cursor 페이지네이션 구현

--- storage/db-core/src/main/java/com/fstuckint/baedalyogieats/storage/db/core/store/StoreRepository.java
+++ storage/db-core/src/main/java/com/fstuckint/baedalyogieats/storage/db/core/store/StoreRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StoreRepository extends JpaRepository<StoreEntity, UUID> {
+public interface StoreRepository extends JpaRepository<StoreEntity, UUID>, StoreRepositoryCustom {
 
     default StoreEntity add(StoreEntity storeEntity) {
         return save(storeEntity);
